5d9680c37eb4ac98b8ce35503ae281fd2bd96222
Cory Sougstad <cory@hau.me>
2021-03-04 08:30:55 -0700

    add netlify config and prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCE-wgAlbsOr0IRU9L1CMNYDrvNM-jJJmE',
+    authDomain: 'flash-recall-b026b.firebaseapp.com',
+    databaseURL: '<your-database-URL>',
+    projectId: 'flash-recall-b026b',
+    storageBucket: 'flash-recall-b026b.appspot.com',
+    messagingSenderId: '783646256937',
+    appId: '1:783646256937:web:9479840b06abae93b85138',
+    measurementId: 'G-5Y7E1HC64Z'
+  }
 };
